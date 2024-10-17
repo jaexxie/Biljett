65efd2919aea8689927e1127f624c550d2405fb2
jaexxie <jasmin.abdi.asker@gmail.com>
2024-10-17 16:17:19 +0200

    exceptions

--- course.py
+++ course.py
@@ -11,12 +11,52 @@ class Course:
         self.courses = []
 
     def add_students(self):
-        name = input("Enter the student's name: ")
-        email = input("Enter the student's email: ")
-        id_number = int(input("Enter the student's ID number: "))
-        major = input("Enter the student's major: ")
-        course_name = input("Enter course name: ")
-        course_code = input("Enter course code: ")
+        while True:
+            name = input("Enter the student's name: ")
+            if name.replace(' ', '').isalpha:
+                name = name.capitalize().strip()
+                break
+            else:
+                print("Invalid input, please enter a valid name. Only letter and spaces are allowed.")
+
+        while True:
+            email = input("Enter the student's email: ")
+            if "@" in email and "." in email:
+                email = email.strip()
+                break
+            else:
+                print("Invalid email format. Please enter a valid email.")
+
+        while True:
+            try:
+                id_number = int(input("Enter the student's ID number: "))
+                break
+            except ValueError:
+                print("Invalid input, please enter a valid numeric ID number.")
+
+        while True:
+            major = input("Enter the student's major: ")
+            if major.replace(' ','').isalpha():
+                major = major.strip().title()
+                break
+            else:
+                print("Invalid input, please enter a valid major. Only letters and spaces are allowed.")
+
+        while True:
+            course_name = input("Enter course name: ")
+            if course_name.replace(' ','').isalpha():
+                course_name = course_name.strip().title()
+                break
+            else:
+                print("Invalid input, please try again. Only letters and spaces are allowed.")
+
+        while True:
+            course_code = input("Enter course code: ")
+            if course_code.isalnum():
+                course_code = course_code.upper().strip()
+                break
+            else:
+                print("Invalid input, please enter a valid alphanumeric course code.")
 
 
         student = Student(name, email, id_number, major, course_name, course_code)
@@ -25,12 +65,53 @@ class Course:
         print(f"Student {name} has been added to the course {course_name}.")
 
     def add_instructors(self):
-        name = input("Enter the instructor's name: ")
-        email = input("Enter the instructor's email: ")
-        id_number = int(input("Enter the instructor's ID number: "))
-        department = input("Enter the instructor's department: ")
-        course_name = input("Enter course name: ")
-        course_code = input("Enter course code: ")
+
+        while True:
+            name = input("Enter the instructors name: ")
+            if name.replace(' ', '').isalpha:
+                name = name.capitalize().strip()
+                break
+            else:
+                print("Invalid input, please enter a valid name. Only letters and spaces are allowed.")
+
+        while True:
+            email = input("Enter the instructor's email: ")
+            if "@" in email and "." in email:
+                email = email.strip()
+                break
+            else:
+                print("Invalid email format. Please enter a valid email.")
+
+        while True:
+            try:
+                id_number = int(input("Enter the instructor's ID number: "))
+                break
+            except ValueError:
+                print("Invalid input, please enter a valid numeric ID number.")
+
+        while True:
+            department = input("Enter the instructor's department: ")
+            if department.replace(' ','').isalpha():
+                department = department.strip().title()
+                break
+            else:
+                print("Invalid input, please enter a valid department. Only letters and spaces are allowed.")
+
+        while True:
+            course_name = input("Enter course name: ")
+            if course_name.replace(' ','').isalpha():
+                course_name = course_name.strip().title()
+                break
+            else:
+                print("Invalid input, please try again. Only letters and spaces are allowed.")
+
+        while True:
+            course_code = input("Enter course code: ")
+            if course_code.isalnum():
+                course_code = course_code.upper().strip()
+                break
+            else:
+                print("Invalid input, please enter a valid alphanumeric course code.")
 
         instructor = Instructor(name, email, id_number, department, course_name, course_code)
 
